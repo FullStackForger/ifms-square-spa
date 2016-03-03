eb61256348e0561b4298095eed0deeb11758f1ea
IndieForger <indieforger@gmail.com>
2016-03-03 00:06:50 +0000

    homepage route added

--- app/square.app.js
+++ app/square.app.js
@@ -1,5 +1,20 @@
 angular
 	.module('squareApp', [
+		'ngRoute',
 		'square.homePage',
 		'square.newsFeed'
 	])
+	.config(configure)
+
+function configure($routeProvider) {
+	"use strict";
+	$routeProvider
+		.when('/homepage', {
+			controller: 'HomePageCtrl',
+			controllerAs: 'homepage',
+			template: '<p>{{homepage.text}}</p>'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}
